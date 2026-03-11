b7a4d46d60ad42a3080c8ad357b7d33ee5eaa614
skatogram <zopapopa885@gmail.com>
2026-03-11 23:41:14 +0300

    Delete fix.ps1

--- fix.ps1
+++
@@ -1,4 +0,0 @@
-$path = 'c:\Users\Виктор\Desktop\ogo\alkad src\invis\core\main\other.hpp'
-$content = [System.IO.File]::ReadAllText($path, [System.Text.Encoding]::GetEncoding("windows-1251"))
-$content = $content.Replace("void validatemanipulatorpos(float maxxzmanipulator, float maxymanipulator)`r`n{", "void validatemanipulatorpos(float maxxzmanipulator, float maxymanipulator)`r`n{`r`n`tif (target_ply == nullptr) return;")
-[System.IO.File]::WriteAllText($path, $content, [System.Text.Encoding]::UTF8)
